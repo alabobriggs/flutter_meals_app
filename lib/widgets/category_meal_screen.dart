41434b15c00bc9fd8e5608a72aa14db1e7c70d2b
alabobriggs <alabobriggs@gmail.com>
2020-07-28 11:52:13 +0100

    add category meals screen

---
+++ lib/widgets/category_meal_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('The recipes'),
+      ),
+      body: Center(
+        child: Text('The recipes for category screen'),
+      ),
+    );
+  }
+}
