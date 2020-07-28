803c925943d419aded76e0bc1eaf84e1eafebd28
alabobriggs <alabobriggs@gmail.com>
2020-07-28 11:46:11 +0100

    move dummy data to data folder

--- lib/widgets/categories_screen.dart
+++ lib/widgets/categories_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:meals_app/dummy_data.dart';
+import 'package:meals_app/data/dummy_data.dart';
 import '../widgets/category_item.dart';
 
 class CategoryScreen extends StatelessWidget {
