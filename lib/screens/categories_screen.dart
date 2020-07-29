eec3a133fbd83c7fcfa41b088e4b3797f02bdb51
alabobriggs <alabobriggs@gmail.com>
2020-07-29 23:13:31 +0100

    remove scafoold from category screen

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -5,26 +5,21 @@ import '../widgets/category_item.dart';
 class CategoryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Meal App'),
+    return GridView(
+      padding: const EdgeInsets.all(25),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 4 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
-      body: GridView(
-        padding: const EdgeInsets.all(25),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 4 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
-        children: DUMMY_CATEGORIES.map((catdata) {
-          return CategoryItem(
-            title: catdata.title,
-            color: catdata.color,
-            id: catdata.id,
-          );
-        }).toList(),
-      ),
+      children: DUMMY_CATEGORIES.map((catdata) {
+        return CategoryItem(
+          title: catdata.title,
+          color: catdata.color,
+          id: catdata.id,
+        );
+      }).toList(),
     );
   }
 }
