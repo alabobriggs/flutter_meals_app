896f6051cd38f67ad565de8fc31f402304099fdb
alabobriggs <alabobriggs@gmail.com>
2020-07-30 21:49:53 +0100

    add button to delete meal

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -87,6 +87,12 @@ class MealDetailScreen extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).pop(mealId);
+        },
+        child: Icon(Icons.delete),
+      ),
     );
   }
 }
--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -9,6 +9,7 @@ class MealItem extends StatelessWidget {
   final int duration;
   final Complexity complexity;
   final Affordability affordability;
+  final Function removeItem;
 
   MealItem({
     @required this.title,
@@ -17,6 +18,7 @@ class MealItem extends StatelessWidget {
     @required this.duration,
     @required this.complexity,
     @required this.affordability,
+    @required this.removeItem,
   });
 
   String get complexityText {
@@ -45,13 +47,19 @@ class MealItem extends StatelessWidget {
     }
   }
 
-  void selectMeal(BuildContext ctx) {
-    Navigator.of(ctx).pushNamed(
+  void selectMeal(BuildContext ctx) async {
+    dynamic result = await Navigator.of(ctx).pushNamed(
       MealDetailScreen.routeName,
       arguments: {
         "id": id,
       },
     );
+
+    if (result != null) {
+      print(result);
+    }
+
+    print(result);
   }
 
   @override
